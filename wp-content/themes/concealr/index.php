99cb2bfe2e32e47a195e3d61e5d8802ec395bc04
Noah Williams <swimjones@SWIMJONES.local>
2017-02-27 21:44:02 -1000

    removing default wp themes

--- wp-content/themes/concealr/index.php
+++ wp-content/themes/concealr/index.php
@@ -2,12 +2,11 @@
 <div id=main>
 <div id=content>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<!--<h2><?php the_title(); ?></h2>
-  <h4><?php the_time(F, js, Y) ?></h4> -->
-  <p><?php the_content(__('more...')); ?></p>
-  <hr> <?php endwhile; else: ?>
+<h2><?php the_title(); ?></h2>
+  <h4><?php the_time(F, js, Y) ?></h4>
+  <p><?php the_content(__('See full story')); ?></p>
+  <hr><?php endwhile; else: ?>
   <p><?php _e('WE CAN\'T FIND THAT POST.');?></p><?php endif; ?>
 </div>
 </div>
-<div id=delimiter></div>
 <?php get_footer();?>
