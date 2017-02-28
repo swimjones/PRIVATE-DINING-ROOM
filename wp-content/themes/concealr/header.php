bdac00a21a05d58b9a34d21d339f7e0e9e1a6c24
Noah Williams <swimjones@SWIMJONES.local>
2017-02-27 22:46:52 -1000

    added index page layout to style.css

--- wp-content/themes/concealr/header.php
+++ wp-content/themes/concealr/header.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url');?>">
-	<link href="https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Amiri|Frank+Ruhl+Libre" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri();?>/js/front.js"></script>
 </head>
--- wp-content/themes/concealr/index.php
+++ wp-content/themes/concealr/index.php
@@ -2,11 +2,16 @@
 <div id=main>
 <div id=content>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<h2><?php the_title(); ?></h2>
-  <h4><?php the_time(F, js, Y) ?></h4>
-  <p><?php the_content(__('See full story')); ?></p>
-  <hr><?php endwhile; else: ?>
-  <p><?php _e('WE CAN\'T FIND THAT POST.');?></p><?php endif; ?>
+		<?php get_the_post_thumbnail(); ?> 
+		<h2 class=index_title><?php the_title(); ?></h2>
+		<p class=index_excerpt><?php the_excerpt(__('See full story')); ?></p>
+	<br>
+	<div class=delimiter></div>
+	<br>
+<?php endwhile; else: ?>
+<p><?php _e('Sorry! We can\'t find that post :-(.');?></p><?php endif; ?>
 </div>
 </div>
 <?php get_footer();?>
+
+
