75a679fea2e5217585f83b010bca254d41a1e683
Noah Williams <swimjones@SWIMJONES.local>
2017-02-27 23:09:57 -1000

    adding image support to index screen

--- wp-content/themes/concealr/index.php
+++ wp-content/themes/concealr/index.php
@@ -2,11 +2,13 @@
 <div id=main>
 <div id=content>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<?php get_the_post_thumbnail(); ?> 
+		<div class=testphoto><?php the_post_thumbnail(); ?>
 		<h2 class=index_title><?php the_title(); ?></h2>
 		<p class=index_excerpt><?php the_excerpt(__('See full story')); ?></p>
+		</div>
 	<br>
 	<div class=delimiter></div>
+	<br>
 	<br>
 <?php endwhile; else: ?>
 <p><?php _e('Sorry! We can\'t find that post :-(.');?></p><?php endif; ?>
