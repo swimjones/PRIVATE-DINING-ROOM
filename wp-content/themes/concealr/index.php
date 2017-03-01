916f98c6f9590721a0b3c40a4ad54bdf55349fd4
Noah Williams <swimk1998@gmail.com>
2017-02-28 22:34:56 -1000

    altered photo class

--- wp-content/themes/concealr/index.php
+++ wp-content/themes/concealr/index.php
@@ -2,7 +2,7 @@
 <div id=main>
 <div id=content>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<div class=testphoto><a href="http://privatediningroom/<?php echo get_post_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+		<div class=feat_img><a href="http://privatediningroom/<?php echo get_post_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
 		<h2 class=index_title><a href="http://privatediningroom/<?php echo get_post_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<p class=index_excerpt><?php the_excerpt(__('See full story')); ?></p>
 		</div>
