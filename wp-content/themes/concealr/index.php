e4ad4c931f0b9d41cbabfb0f6c807009f4c59999
Noah Williams <swimjones@SWIMJONES.local>
2017-02-28 22:25:42 -1000

    title media query + single post page

--- wp-content/themes/concealr/index.php
+++ wp-content/themes/concealr/index.php
@@ -2,8 +2,8 @@
 <div id=main>
 <div id=content>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<div class=testphoto><?php the_post_thumbnail(); ?>
-		<h2 class=index_title><?php the_title(); ?></h2>
+		<div class=testphoto><a href="http://privatediningroom/<?php echo get_post_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+		<h2 class=index_title><a href="http://privatediningroom/<?php echo get_post_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<p class=index_excerpt><?php the_excerpt(__('See full story')); ?></p>
 		</div>
 	<br>
@@ -15,5 +15,3 @@
 </div>
 </div>
 <?php get_footer();?>
-
-
---
+++ wp-content/themes/concealr/single.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<div id=main>
+<div id=content>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<p class=index_excerpt><?php the_content(); ?></p>
+		</div>
+	<br>
+	<div class=delimiter></div>
+	<br>
+	<br>
+<?php endwhile; else: ?>
+<p><?php _e('Sorry! We can\'t find that post :-(.');?></p><?php endif; ?>
+</div> <!-- #main div --> 
+</div> <!-- #wrapper div --> 
+<?php get_footer();?>
