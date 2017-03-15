6d7f224e8d3ad3af8c4efb0f721133823c4ea80b
swimjones <swimk1998@gmail.com>
2017-03-14 22:03:08 -1000

    added disappearing mobile menu

--- wp-content/themes/concealr/js/front.js
+++ wp-content/themes/concealr/js/front.js
@@ -1,5 +1,6 @@
+console.log("The future is out there!");
 
-// opening and closing nav menu
+// START CLOSING + OPENING NAV MENU // ---------------------->
 
 function navlistopen(){
 	var nav = document.getElementById("nav_content");
@@ -15,10 +16,47 @@ function navlistclose(){
 	nav.className = "nav_list";
 }
 
-// begin hidden menu
+// START CLOSING + OPENING NAV MENU // ---------------------->
+
+// START HIDE MENU ON SCROLL // ---------------------->
+
+var didScroll;
+var lastScrollTop 	= 0; 
+var navbarHeight 	= $("#header").outerHeight();
+var delta 			= 5; 
+
+$(window).scroll(function(event){
+	didScroll = true;
+})
+
+setInterval(function(){
+	if(didScroll){
+		hasScrolled();
+		didScroll = false;
+	}
+}, 250)
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    if (st < lastScrollTop){
+        $('#header').show();
+        console.log("showed menu");
+    } else {
+        if(st + $(window).height() < $(document).height()) {
+            $('#header').hide();
+            console.log("removed class");
+        }
+    }
+    
+    lastScrollTop = st;
+}
 
 
-
+// END HIDE MENU ON SCROLL // ---------------------->
 
 
 
