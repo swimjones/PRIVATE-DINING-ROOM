a2a8c14f7176a6ec0a2f6bd5a947031a82af6d11
swimjones <swimk1998@gmail.com>
2017-03-25 23:09:25 -1000

    add resources folder

---
+++ wp-content/themes/concealr/resources/js/front.js
@@ -0,0 +1,46 @@
+console.log("The future is out there!");
+
+// START CLOSING + OPENING NAV MENU // ---------------------->
+
+// START CLOSING + OPENING NAV MENU // ---------------------->
+
+// START HIDE MENU ON SCROLL // ---------------------->
+
+var didScroll;
+var lastScrollTop 	= 0; 
+var navbarHeight 	= $("#header").outerHeight();
+var delta 			= 5; 
+
+$(window).scroll(function(event){
+	didScroll = true;
+})
+
+setInterval(function(){
+	if(didScroll){
+		hasScrolled();
+		didScroll = false;
+	}
+}, 250)
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    if (st < lastScrollTop){
+        $('#header').show();
+    } else {
+        if(st + $(window).height() < $(document).height()) {
+            $('#header').hide();
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+
+// END HIDE MENU ON SCROLL // ---------------------->
+
+
+
