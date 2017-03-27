9a4c6b763137c300a3749e165e6dc6f3c6ca1321
swimjones <swimk1998@gmail.com>
2017-03-27 02:00:06 -1000

    fixed mobile menu -- still open, though.

--- wp-content/themes/concealr/header.php
+++ wp-content/themes/concealr/header.php
@@ -3,23 +3,21 @@
 <head>
 	<title>PRIVATE DINING ROOM</title>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url');?>">
-	<link href="https://fonts.googleapis.com/css?family=Amiri|Frank+Ruhl+Libre" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"/>
+	<link href="https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri();?>/js/front.js"></script>
+	<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri();?>/resources/js/front.js"></script>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url');?>">
 </head>
 <div id=wrapper>
 <div id=header>
 	<h1><a href="http://privatediningroom/">PRIVATE DINING ROOM</a></h1>
-		<button><a id=top_icon href="javascript:void(0)" onclick="navlistopen()">&#9776;</button>
-		<ul class=nav_list id=nav_content>
-			<li><a href="#">events</a></li>
-			<li><a href="#">magazine</a></li>
-			<li><a href="#">blog</a></li>
-			<li><a href="#">about</a></li>
-			<li><a href="javascript:void(0)" onclick="navlistclose()">exit</a></li>
-		</ul>
+	<a id=pipes_button href="javascript:void(0)" onclick=""><img src="<?php echo get_stylesheet_directory_uri();?>/resources/svg/menu_pipes.svg"></a>
+	<ul class=show_menu>
+		<li><a id=mag href="#">magazine</a></li>
+		<li><a id=extras href="#">extras</a></li>
+		<li><a id=info href="#">info</a></li>
+	</ul>
 </div>
 
 
--- wp-content/themes/concealr/index.php
+++ wp-content/themes/concealr/index.php
@@ -4,16 +4,6 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class=content_block>
 			<a href="http://privatediningroom/<?php echo get_post_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-			<div id=index_info>
-				<h3 class=index_category></h3>
-				<h2 class=index_title>
-					<a href="http://privatediningroom/<?php echo get_post_permalink(); ?>"><?php the_title(); ?></a>
-				</h2>
-				<p class=index_excerpt><?php the_excerpt(); ?></p>
-				<button class=read_more>
-					<a href="http://privatediningroom/<?php echo get_post_permalink(); ?>">Read more</a>
-				</button>
-			</div> <!-- .index_info end -->
 		</div> <!-- .feat_img end -->
 		<br><br>
 <?php endwhile; else: ?>
