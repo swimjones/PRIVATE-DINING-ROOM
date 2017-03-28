3f5d0d969b5b08194abe18e11d5c264114c9217c
swimjones <swimk1998@gmail.com>
2017-03-28 09:00:50 -1000

    fixed line-up issue on menu foldout

--- wp-content/themes/concealr/index.php
+++ wp-content/themes/concealr/index.php
@@ -2,10 +2,6 @@
 <div id=main>
 <div id=content>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<div class=content_block>
-			<a href="http://privatediningroom/<?php echo get_post_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-		</div> <!-- .feat_img end -->
-		<br><br>
 <?php endwhile; else: ?>
 <p><?php _e('Sorry! We can\'t find that post :-(.');?></p><?php endif; ?>
 </div> <!-- #content end -->
