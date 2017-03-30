01589deba3526c92e78ce286d7c85251b0919b01
swimjones <swimk1998@gmail.com>
2017-03-29 16:31:22 -1000

    wordpress update

--- wp-content/themes/concealr/index.php
+++ wp-content/themes/concealr/index.php
@@ -4,7 +4,14 @@
 <div id=post_block>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<?php echo get_the_post_thumbnail(); ?>
-			<h2><?php echo the_title(); ?></h2>
+			<div class=post_box>
+				<div class=vert_bar></div>
+				<div class=post_content>
+					<h2><?php echo the_title(); ?></h2>
+					<br>
+					<p><?php echo the_excerpt();?></p>
+				</div>
+			</div>
 	<?php endwhile; else: ?>
 </div>
 <p><?php _e('Sorry! We can\'t find that post :-(.');?></p><?php endif; ?>
