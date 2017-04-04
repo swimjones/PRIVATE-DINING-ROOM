39f87dbeb388658e539988dfd62e77f4f4f2e18a
swimjones <swimk1998@gmail.com>
2017-04-04 13:47:04 -1000

    fixed hovering links

--- wp-content/themes/concealr/footer.php
+++ wp-content/themes/concealr/footer.php
@@ -1,5 +1,6 @@
 <div id=footer>
 <p>PRIVATE DINING ROOM is a production company focused on brand development and the relationship between physical and digital space.</p>
+<p>Got ideas? Contact us at info@salonprivada.com</p>
 </div> <!-- end of #footer --> 
 </div> <!-- end of #wrapper -->
 </body>
