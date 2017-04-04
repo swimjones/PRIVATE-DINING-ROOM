4d542da7032d8ed788fcb6a50e8871b596482b12
swimjones <swimk1998@gmail.com>
2017-04-04 13:21:31 -1000

    fixed footer

--- wp-content/themes/concealr/footer.php
+++ wp-content/themes/concealr/footer.php
@@ -1,5 +1,4 @@
 <div id=footer>
-<h2></h2>
 <p>PRIVATE DINING ROOM is a production company focused on brand development and the relationship between physical and digital space.</p>
 </div> <!-- end of #footer --> 
 </div> <!-- end of #wrapper -->
