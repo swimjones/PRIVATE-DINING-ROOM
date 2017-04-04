eaa2eb9a44e7a56c0f4db8d1d10e66000af3dddf
swimjones <swimk1998@gmail.com>
2017-04-04 13:12:57 -1000

    fixed ATF content -- responsive version

--- wp-content/themes/concealr/single.php
+++ wp-content/themes/concealr/single.php
@@ -3,15 +3,17 @@
 <div id=content>
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div id=post_atf>
-			<h1><?php the_title(); ?></h1>
-			<br>
-			<p class=atf_info class=thin-helv>Story by <?php the_author();?></p>
-			<p class=atf_info class=thin-helv><?php the_date();?></p>
-			<br><br>
-			<?php echo the_excerpt();?>
-			<br><br>
-			<p><a href="#single_article">Read more</a></p>
-			<br><br>
+			<div id=atf_content>
+				<h1><?php the_title(); ?></h1>
+				<br>
+				<p class=atf_info class=thin-helv>Story by <?php the_author();?></p>
+				<p class=atf_info class=thin-helv><?php the_date();?></p>
+				<br><br>
+				<?php echo the_excerpt();?>
+				<br><br>
+				<p><a href="#single_article">Read more</a></p>
+				<br><br>
+			</div>
 		</div>
 		<div id=single_article>
 			<?php the_content(); ?>
