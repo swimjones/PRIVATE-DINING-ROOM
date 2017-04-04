51904d1577e9aa1e86b8966be84f922dfb294569
swimjones <swimk1998@gmail.com>
2017-04-03 16:45:10 -1000

    added footer + draft statement

--- wp-content/themes/concealr/footer.php
+++ wp-content/themes/concealr/footer.php
@@ -1,5 +1,6 @@
 <div id=footer>
-<h1>world class information</h1>
+<h2></h2>
+<p>PRIVATE DINING ROOM is a production company focused on helping our clients navigate brand development across physical and digital space.</p>
 </div> <!-- end of #footer --> 
 </div> <!-- end of #wrapper -->
 </body>
--- wp-content/themes/concealr/index.php
+++ wp-content/themes/concealr/index.php
@@ -6,7 +6,7 @@
 			<a href="<?php echo get_post_permalink();?>"><?php echo get_the_post_thumbnail(); ?></a>
 				<div class=post_content>
 					<div class=vert_bar></div>
-					<h2><a href="<?php echo get_post_permalink();?>"><?php echo the_title(); ?></a></h2>
+					<h2><a class=title_link href="<?php echo get_post_permalink();?>"><?php echo the_title(); ?></a></h2>
 					<p class=date><?php echo the_date(); ?> — <?php echo the_time(); ?></p>
 					<p><?php echo the_excerpt();?></p>
 				</div>
