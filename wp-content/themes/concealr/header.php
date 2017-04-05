d726eac6bec70c352eeab2916834eeb6869c3641
swimjones <swimk1998@gmail.com>
2017-04-04 17:50:34 -1000

    added tweet buttons + dynamic links

--- wp-content/themes/concealr/header.php
+++ wp-content/themes/concealr/header.php
@@ -6,7 +6,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"/>
 	<link href="https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri();?>/resources/js/front.js"></script>
+	<script src="<?php echo get_stylesheet_directory_uri();?>/resources/js/front.js"></script>
+	<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url');?>">
 </head>
 <div id=wrapper>
