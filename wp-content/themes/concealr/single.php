9f65c227dba5264756056a459fed5ab9a6460378
swimjones <swimk1998@gmail.com>
2017-04-04 18:27:04 -1000

    added space to atf

--- wp-content/themes/concealr/single.php
+++ wp-content/themes/concealr/single.php
@@ -5,15 +5,15 @@
 		<div id=post_atf>
 			<div id=atf_content>
 				<h1><?php the_title(); ?></h1>
-				<br>
-				<p class=atf_info class=thin-helv>Story by <?php the_author();?></p>
-				<p class=atf_info class=thin-helv><?php the_date();?></p>
+				<br><br>
+				<p class=thin-helv>Story by <?php the_author();?></p>
+				<p class=thin-helv><?php the_date();?></p>
 				<br><br>
 				<?php echo the_excerpt();?>
 				<br><br>
-				<p><a href="#single_article">Read more</a></p>
+				<p><a id=readmore href="#single_article">Read more</a></p>
 				<br><br>
-				<p><a href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-show-count="false">Tweet</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script></p>
+				<p><a href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-show-count="false">Tweet</a></p>
 				<br><br>
 			</div>
 		</div>
