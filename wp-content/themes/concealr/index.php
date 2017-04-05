1aa5c2fe3e311d2fdae45aea293f12a5eb4c8696
swimjones <swimk1998@gmail.com>
2017-04-04 17:38:15 -1000

    added social to index page

--- wp-content/themes/concealr/index.php
+++ wp-content/themes/concealr/index.php
@@ -8,6 +8,7 @@
 					<h2><a class=title_link href="<?php echo get_post_permalink();?>"><?php echo the_title(); ?></a></h2>
 					<p class=thin-helv class=date style="font-style:italic;"><?php echo the_date(); ?> — <?php echo the_time(); ?></p>
 					<p><?php echo the_excerpt();?></p>
+					<p><a href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-show-count="false">Tweet</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script></p>
 				</div>
 			</div>
 	<?php endwhile; else: ?>
