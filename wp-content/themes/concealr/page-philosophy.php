644b6e43811f174e40d76e4f4dfe24e84dc2801a
swimjones <swimk1998@gmail.com>
2017-04-10 22:20:24 -1000

    added philosophy page

---
+++ wp-content/themes/concealr/page-philosophy.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+	<div id=philo_atf>
+		<p>We are a small, focused collective promoting productive escapism. <br><br>We create spaces, craft experiences, and tell stories that connect people together, start conversations, and encourage emotional engagement.</p>
+	</div>
+<?php get_footer(); ?>
