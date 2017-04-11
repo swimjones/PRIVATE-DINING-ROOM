794c109232318c952f4bb8d709271a8e1e1b35e0
swimjones <swimk1998@gmail.com>
2017-04-11 06:50:15 -1000

    fixed 320px page

--- wp-content/themes/concealr/page-philosophy.php
+++ wp-content/themes/concealr/page-philosophy.php
@@ -2,4 +2,3 @@
 	<div id=philo_atf>
 		<p>We are a small, focused collective promoting productive escapism. <br><br>We create spaces, craft experiences, and tell stories that connect people together, start conversations, and encourage emotional engagement.</p>
 	</div>
-<?php get_footer(); ?>
