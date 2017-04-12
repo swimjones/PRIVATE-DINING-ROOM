e03b5506063f7f8818c19794cb95fd147e17b3fa
swimjones <swimk1998@gmail.com>
2017-04-11 17:49:45 -1000

    updated footer

--- wp-content/themes/concealr/footer.php
+++ wp-content/themes/concealr/footer.php
@@ -1,5 +1,5 @@
 <div id=footer>
-<p>Got a tip? Contact us at <span style="text-decoration:underline;">info@privatediningroom.com</span></p>
+<p>Thank you for reading.</p>
 </div> <!-- end of #footer --> 
 </div> <!-- end of #wrapper -->
 </body>
