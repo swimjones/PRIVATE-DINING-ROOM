72b496e78e2febe220f639dd29562f2377012653
swimjones <nwilliams17@punahou.edu>
2017-04-12 19:58:00 -1000

    wpconfig

--- wp-config.php
+++ wp-config.php
@@ -38,7 +38,7 @@ define('DB_PASSWORD', $password);
 define('DB_HOST', $database);
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
