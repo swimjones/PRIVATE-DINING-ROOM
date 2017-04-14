9b52fd5ef0ea85aed946f2da3dc7db030f3559c5
swimjones <nwilliams17@punahou.edu>
2017-04-13 19:04:28 -1000

    fixed header
    fixed header links
    
    fixed htaccess
    
    updated gitignore
    
    updated gitignore
    
    update gitignore 3
    
    test script
    
    remove test script
    
    final commit before squash
    
    re-add .htaccess
    
    compressed css
    
    test ht
    
    ht update
    
    test exclude
    
    revert htaccess
    
    httest
    
    test
    
    removed new.txt
    
    new.txt
    
    updated header links

--- wp-content/themes/concealr/header.php
+++ wp-content/themes/concealr/header.php
@@ -5,22 +5,20 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"/>
 	<link href="https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre|Source+Serif+Pro:700" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url');?>">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="<?php echo get_stylesheet_directory_uri();?>/js/front.js"></script>
 	<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url');?>">
 </head>
 <div id=wrapper>
 <div id=header class=default>
 	<div id=header_nav>
-		<h1><a href="<?php getenv('DOMAIN_NAME'); ?>">PRIVATE DINING ROOM</a></h1>
+		<h1><a href="http://<?php echo getenv('DOMAIN_NAME')?>">PRIVATE DINING ROOM</a></h1>
 		<a id=pipes_button href="javascript:void(0)" onclick="bye_menu();">&#9776;</a>
 	</div>
 	<ul class=no_menu>
-		<li><a id=mag href="<?php getenv('DOMAIN_NAME'); ?>">our ideas/<span class=menu_desc>what we think</span></a></li>
+		<li><a id=mag href="http://<?php echo getenv('DOMAIN_NAME')?>">our ideas/<span class=menu_desc>what we think</span></a></li>
 		<li><a id=extras href="products">our products/<span class=menu_desc>what we do</span></a></li>
 		<li><a id=info href="philosophy">our philosophy/<span class=menu_desc>who we are</span></a></li>
 	</ul>
 </div>
-
-
