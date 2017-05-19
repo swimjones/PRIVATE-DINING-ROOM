16b6ae67fc3fcaf338d7208d9b0d879d0bca195d
swimjones <nwilliams17@punahou.edu>
2017-05-19 12:04:03 -1000

    start about page

--- wp-content/themes/concealr/page-products.php
+++
@@ -1,4 +0,0 @@
-<?php get_header(); ?>
-<div id=philo_atf>
-	<p>Our offerings include social events, educational lectures, and hands-on workshops that support our partners and provide a platform for meaningful audience interaction.<br><br>Whether through online interaction, physical presence, or purchase-driven engagement, our goal is always the same: to craft thoughtful, engaging brand experiences.</p>
-</div>
